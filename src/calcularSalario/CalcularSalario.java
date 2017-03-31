36132d3a946e1ff3bbc26588e3723d9298083821
Geoguitar <geovanedsilveira@gmail.com>
2017-03-31 00:23:18 -0300

    adicionando arquivos

---
+++ src/calcularSalario/CalcularSalario.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcularSalario;
+
+/**
+ *
+ * @author geoguitar
+ */
+public class CalcularSalario {
+    
+ /**
+ *
+ *  int salario;
+    int salarioHora;
+    int numHoras = 0;
+ * 
+ */
+    
+    
+    public CalcularSalario(){
+    
+           initComponents();
+           
+        //Chamar o método do botão btnCalcularSalario ao selecionar o Enter
+         //getRootPane().setDefaultButton(btnCalcularSalario);
+           
+    }
+
+    private void initComponents() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
